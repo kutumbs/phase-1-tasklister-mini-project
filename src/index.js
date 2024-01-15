b948cd7ab6d8bbda17f656381b239f573c4a1df9
jonathan <jonathan.123kutumbakana@gmail.com>
2024-01-15 22:22:09 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector("form")
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    input =  document.getElementById('new-task-description').value
+    handleTodos(input)
+    form.reset()
+  });
 });
+
+function handleTodos(answer) {
+  let newTodos = document.createElement('li')
+  let btn = document.createElement('button')
+  btn.addEventListener("click", handleDelete)
+  btn.textContent = "X"
+  newTodos.textContent = answer
+  newTodos.appendChild(btn)
+  document.querySelector('#tasks').appendChild(newTodos)
+  console.log(answer)
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove()
+}
+
